fa3aa8432b9182b95cd0c708e11d48e8e375f4ef
Sowmini Vemajala <sowminivemajala@Sowminis-MBP.attlocal.net>
2023-06-07 23:06:58 -0400

    initial commit for onesite

---
+++ src/main/java/com/onesiteapp/onesite/OnesiteApplication.java
@@ -0,0 +1,13 @@
+package com.onesiteapp.onesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnesiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onesiteapp/onesite/OnesiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onesiteapp.onesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnesiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
